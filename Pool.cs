487924280a155f7b1bc1e382bf98c0798e187728
dioufcs <34384776+dioufcs@users.noreply.github.com>
2020-03-11 17:17:24 -0400

    Add files via upload

---
+++ Pool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Pool
+{
+    //public List<IComponent> pool;
+    public IComponent[] pool;
+
+
+    public Pool()
+    {
+       pool = new IComponent[ComponentsManager.maxEntities];
+    }
+
+    public void SetComponent(EntityComponent entityID, IComponent component)
+    {
+        pool[entityID.id] = component;
+    }
+public void RemoveComponent(EntityComponent entityID)
+    {
+        pool[entityID.id] = null;
+    }
+
+    public IComponent GetComponent(EntityComponent entityID)
+    {
+        //Debug.Log( componentIndex.ToString() + " " + sequentialPool.Count.ToString());
+        //Debug.Log(sequentialPool[0].GetType().Name);
+        return pool[entityID.id];
+
+    }
+
+    public void Clear()
+    {
+        Array.Clear(pool, 0, 1100);
+        //IndirectionTable = new int?[ComponentsManager.maxEntities];
+        //pool.Clear();
+    }
+
+
+}
